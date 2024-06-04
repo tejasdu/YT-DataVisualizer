32ae850fabae2bee26e4cc276932aa88c475cd09
tejasdu <tejasd2022@gmail.com>
2024-06-03 22:41:44 -0400

    Second

--- main.py
+++ main.py
@@ -13,19 +13,29 @@ api_version = "v3"
 # Get credentials and create an API client
 youtube = build(api_service_name, api_version, developerKey=api_key)
 
-username_list = input("Please enter the usernames of the channels you would like to analyze, seperated by a single space between each one\n")
+username_list = input("Please enter the usernames of the channels you would like to analyze, seperated by a single space between each one.\nMAKE SURE THE USERNAMES ARE"
+                      " SPELLED CORRECTLY. There are over 114 million youtube channels, so this program will not be able to diffrentiate between whether there the user inputed channels have a typo or are valid names:\n")
 channels = username_list.split()
 
-if len(channels) == 0:
-    print("No usernames were provided. Please pprovide a list valid usernames seperated by a single space")
+
+if len(channels) < 1:
+    print("No usernames were provided. Please provide a list valid usernames seperated by a single space.\n")
     sys.exit(1)
 
-print(channels)
+responses = []
 
-request = youtube.channels().list(
-    part="snippet,contentDetails,statistics",
-    forUsername="khanacademy"
-)
+for channel in channels:
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        forUsername=channel
+    )
 
-response = request.execute()
-print(response)
+    response = request.execute()
+    responses.append(response)
+
+for i, response in enumerate(responses):
+    print(f"Extracting data for the channel: {channels[i]}: ")
+    print(response)
+    print()
+
+
